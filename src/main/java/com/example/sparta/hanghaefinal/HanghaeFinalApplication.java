b6351f3b230967e9e123d08ab188c651b0c39f55
ggongdori <soominlee0410@gmail.com>
2022-05-02 10:37:22 +0900

    initial commit

---
+++ src/main/java/com/example/sparta/hanghaefinal/HanghaeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparta.hanghaefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sparta/hanghaefinal/HanghaeFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparta.hanghaefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
