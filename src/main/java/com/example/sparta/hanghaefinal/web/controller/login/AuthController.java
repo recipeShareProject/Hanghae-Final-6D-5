22aedb987c3bee79b9e3aea428f962e3260cbedc
ggongdori <soominlee0410@gmail.com>
2022-05-17 16:19:26 +0900

    last commit before merge

--- src/main/java/com/example/sparta/hanghaefinal/web/controller/login/AuthController.java
+++ src/main/java/com/example/sparta/hanghaefinal/web/controller/login/AuthController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
