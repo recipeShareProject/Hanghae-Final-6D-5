2dc9f47e8fea7f733aae004e8ee3c1dd4da308ed
한상훈 <ahan0421@gmail.com>
2022-05-23 17:19:36 +0900

    알람 초기 작업 엔티티설정

--- src/main/java/com/hanghae/justpotluck/domain/alarm/entity/Alarm.java
+++ src/main/java/com/hanghae/justpotluck/domain/alarm/entity/Alarm.java
@@ -2,61 +2,41 @@ package com.hanghae.justpotluck.domain.alarm.entity;
 import com.hanghae.justpotluck.domain.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hanghae.justpotluck.global.config.Timestamped;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
+@Builder
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Alarm extends Timestamped {
-    @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "message_id")
-    private Long alarmId;
-//알람 아이디
-    @JsonManagedReference
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "receiver_id")
-    private User receiver;
-//알람 받는 사람
-    @Column(name = "sender_nickName")
-    private String senderNickName;
-//알람 보낸 사람
-    @Column
-    @Enumerated(EnumType.STRING)
-    private AlarmType alarmType;
-//알림 종류 보드인지 매거진인지 전체 공지인지 홍보 알림인지 등등
-    @Column
+    @Id
     private Long id;
 
+    @Column
+    private Long userId; // 알람 받는 대상
+
+    @Column(name="sender_nickname")
+    private String senderNickName;
 
     @Column
-    private String title;
+    @Enumerated(value = EnumType.STRING)
+    private AlarmType type;
 
-    @Enumerated(EnumType.STRING)
-    private  FReadingStatus readingStatus;
+    @Column
+    private Long pubId; // 채팅방 생성자
 
+    @Column
+    private Long postId; // 게시물 번호
 
-    public Alarm(User receiver, String senderNickName, AlarmType alarmType, Long id, String title) {
-        this.receiver = receiver;
-        this.senderNickName = senderNickName;
-        this.alarmType = alarmType;
-        this.id = id;
-        this.title = title;
-        this.readingStatus = ReadingStatus.N;
-    }
+    @Column
+    private String alarmMessage;
 
-
-    public void changeReadingStatus(ReadingStatus readingStatus) {
-        this.readingStatus = readingStatus;
-    }
-
-    public LocalDateTime modifiedAt() {
-        return null;
-    }
+    @Column
+    private Boolean isRead;
 }
---
+++ src/main/java/com/hanghae/justpotluck/domain/alarm/entity/AlarmType.java
@@ -0,0 +1,11 @@
+package com.hanghae.justpotluck.domain.alarm.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+
+public enum AlarmType {
+
+    recipe_comment,
+    community_comment
+}
